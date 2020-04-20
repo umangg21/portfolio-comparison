1adfed276f2640e79d7543cdece6fff0a4204986
Umang <umang@indwealth.in>
2020-04-20 14:57:04 +0530

    Updates

---
+++ precache-manifest.0610efaecbcf3dfaa6c64a4dd46f6be9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4483b64f41f5e37643e2fcfebd2c72c",
+    "url": "/portfolio-comparison/index.html"
+  },
+  {
+    "revision": "5a8eac7c209f074afcc4",
+    "url": "/portfolio-comparison/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a2fa588cc8c5db2ebb99",
+    "url": "/portfolio-comparison/static/css/main.26e81656.chunk.css"
+  },
+  {
+    "revision": "5a8eac7c209f074afcc4",
+    "url": "/portfolio-comparison/static/js/2.5f3a4dc9.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/portfolio-comparison/static/js/2.5f3a4dc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2fa588cc8c5db2ebb99",
+    "url": "/portfolio-comparison/static/js/main.a73dc5c6.chunk.js"
+  },
+  {
+    "revision": "04ba38d578e95d4b989a",
+    "url": "/portfolio-comparison/static/js/runtime-main.c2219d43.js"
+  }
+]);
